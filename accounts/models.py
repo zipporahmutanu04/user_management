94ef49f0090ff88d968ea45793673180ca0774b7
brian <zipporahmutanu04@gmail.com>
2025-07-07 12:01:01 +0300

    Add UserProfile model
    Created UserProfile model with bio and is_verified fields. Registered model in admin. Generated and applied migrations.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
