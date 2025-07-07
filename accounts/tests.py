7be014cbd73a719360aa8922a2b57b4bd85acdb9
brian <zipporahmutanu04@gmail.com>
2025-07-07 14:30:14 +0300

    Add unit tests
    Implemented unit tests for UserProfile model and views (register, profile, edit_profile). Tests verify model creation and view accessibility.

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import UserProfile
+from django.urls import reverse
 
-# Create your tests here.
+class UserProfileModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='zazima', password='1234', email='mutanuzippy04@gmail.com')
+        self.profile = UserProfile.objects.create(user=self.user, bio='Test bio')
+
+    def test_user_profile_creation(self):
+        self.assertEqual(self.profile.user.username, 'zazima')
+        self.assertEqual(self.profile.bio, 'Test bio')
+        self.assertFalse(self.profile.is_verified)
+
+class UserViewsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='zazima', password='1234', email='mutanuzippy04@gmail.com')
+        self.profile = UserProfile.objects.create(user=self.user, bio='Test bio')
+
+    def test_register_view(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/register.html')
+
+    def test_profile_view(self):
+        self.client.login(username='zazima', password='1234')
+        response = self.client.get(reverse('profile'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profile/profile.html')
+
+    def test_edit_profile_view(self):
+        self.client.login(username='zazima', password='1234')
+        response = self.client.get(reverse('edit_profile'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profile/edit_profile.html')
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
 from .forms import RegistrationForm, ProfileForm
 from .models import UserProfile
 
